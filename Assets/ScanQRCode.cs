a5c173ff24bebdec62d32d04633c444643bf33b3
linminquan <leolmq@gmail.com>
2016-07-24 16:53:47 +0800

    fullscreen camera; compatiable with ios rotation;

--- Assets/ScanQRCode.cs
+++ Assets/ScanQRCode.cs
@@ -7,32 +7,72 @@ public class ScanQRCode : MonoBehaviour
 {
 	private WebCamTexture webCamTexture;
 	private string resultText;
+	private Material quadMat;
 
 	// Use this for initialization
 	void Start () 
 	{
-		webCamTexture = new WebCamTexture (640, 480);
+		webCamTexture = new WebCamTexture (4096, 4096);
 		WebCamDevice[] devices = WebCamTexture.devices;
 		webCamTexture.deviceName = devices[0].name;
+		quadMat = GetComponent<Renderer>().material;
+		quadMat.mainTexture = webCamTexture;
 		webCamTexture.Play ();
 
 		InvokeRepeating ("Scan", 1f, 1f);
 	}
 
-	void OnGUI()  
-	{  
-		GUI.DrawTexture(new Rect(0, 0, 640, 480), webCamTexture, ScaleMode.ScaleToFit);  
-	} 
+	void Update()
+	{
+		transform.rotation = Quaternion.AngleAxis(webCamTexture.videoRotationAngle, -Vector3.forward);
+
+		var screenAspect = (float)Screen.width / Screen.height;
+		var webCamAspect = (float)webCamTexture.width / webCamTexture.height;
+		//Debug.Log ("screenAspect: " + screenAspect + " " + "webCamAspect " + webCamAspect);
+		//Debug.Log ("webCam.width: "+webCamTexture.width+" "+"webCam.height: "+webCamTexture.height);
+
+		var rot90 = (webCamTexture.videoRotationAngle / 90) % 2 != 0;
+		//Debug.Log ("rot90: " + rot90);
+		if (rot90) 
+		{
+			webCamAspect = 1.0f / webCamAspect;
+		}
+		//Debug.Log ("webCamAspect " + webCamAspect); //0.5625
+
+		float sx, sy;
+		if (webCamAspect < screenAspect) //0.5625 > 0.5622189
+		{
+			sx = webCamAspect;
+			sy = 1.0f;
+		}
+		else
+		{
+			sx = screenAspect;
+			sy = screenAspect / webCamAspect;
+		}
+		//Debug.Log ("sx "+sx+" sy "+sy);
+
+		if (rot90) 
+		{
+			transform.localScale = new Vector3 (sy, sx, 1);
+		} 
+		else 
+		{
+			transform.localScale = new Vector3 (sx, sy, 1);
+		}
+			
+		var mirror = webCamTexture.videoVerticallyMirrored;
+		//Debug.Log ("mirror " + mirror);
+		quadMat.mainTextureOffset = new Vector2(0, mirror ? 1 : 0);
+		quadMat.mainTextureScale = new Vector2(1, mirror ? -1 : 1);
+	}
 
 	private void Scan()
 	{
-		if (string.IsNullOrEmpty(resultText)) 
+		if (webCamTexture != null && webCamTexture.width > 100) 
 		{
-			if (webCamTexture != null && webCamTexture.width > 100) 
-			{
-				resultText = Decode(webCamTexture.GetPixels32 (), webCamTexture.width, webCamTexture.height);
-				Debug.Log (resultText);
-			}
+			resultText = Decode(webCamTexture.GetPixels32 (), webCamTexture.width, webCamTexture.height);
+			Debug.Log (resultText);
 		}
 	}
 
@@ -45,5 +85,14 @@ public class ScanQRCode : MonoBehaviour
 			return result.Text;
 		}
 		return null;
+	}
+
+	void OnGUI()
+	{
+		var text = "web cam size = " + webCamTexture.width + " x " + webCamTexture.height;
+		text += "\nrotation = " + webCamTexture.videoRotationAngle;
+		text += "\nscreen size = " + Screen.width + " x " + Screen.height;
+		text += "\nresultText = " + resultText;
+		GUI.Label(new Rect(0, 0, Screen.width, Screen.height), text);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ZXing_Unity3D_Test
+
+ZXing.Net 0.14.0.0
+https://zxingnet.codeplex.com
+
+Unity3D 5.3.5f1
+Mac OS X 10.11.5 
